0e0c610d8b4199b7f9b7e2adda869d2edc0411b1
pooja <shah.developer9@gmail.com>
2020-10-31 14:54:06 +0530

    initial

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-const getPairs = function (arr, sum) {
+const getpairofSum = function (arr, sum) {
   for (i = 0; i < arr.length; i++) {
     for (j = i + 1; j < arr.length; j++) {
       if (arr[i] + arr[j] == sum) {
-        console.log("(" + arr[i] + "," + arr[j] + ")");
+         console.log("(" + arr[i] + "," + arr[j] + ")");
       }
     }
   }
-
 };
 
-const arr = [1, 5, 7, -1, 5];
-const sum = 6;
-getPairsCount(arr, sum);
+const arr = [10, 20, 40, 50, 60, 70, -10];
+const sum = 50;
+getpairofSum(arr, sum);
